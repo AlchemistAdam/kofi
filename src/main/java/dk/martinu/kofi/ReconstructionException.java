8185826b77522f99d34f913563a9686904b45962
Adam Martinu <1337programmering.c@gmail.com>
2022-03-04 17:25:17 +0100

    Added exception.

---
+++ src/main/java/dk/martinu/kofi/ReconstructionException.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022, Adam Martinu. All rights reserved. Altering or
+ * removing copyright notices or this file header is not allowed.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");  you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package dk.martinu.kofi;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * Thrown when an exception is caught while reconstructing an array or object.
+ *
+ * @author Adam Martinu
+ * @see KofiArray#reconstruct(Class)
+ * @see KofiObject#reconstruct(Class)
+ * @since 1.0
+ */
+public class ReconstructionException extends RuntimeException implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 0L;
+
+    /**
+     * Constructs a new exception with the specified message and cause.
+     */
+    @Contract(pure = true)
+    public ReconstructionException(final String msg, final Throwable cause) {
+        super(msg, cause);
+    }
+}
