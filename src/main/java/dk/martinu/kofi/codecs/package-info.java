da7c30f2e798a5f0a8c1310d08b8da18a27cd0fc
Adam Martinu <1337programmering.c@gmail.com>
2022-03-04 17:16:12 +0100

    Added package info.

---
+++ src/main/java/dk/martinu/kofi/codecs/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022, Adam Martinu. All rights reserved. Altering or
+ * removing copyright notices or this file header is not allowed.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");  you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Provides codecs for reading and writing
+ * {@link dk.martinu.kofi.Document Document} objects.
+ *
+ * @author Adam Martinu
+ * @since 1.0
+ */
+package dk.martinu.kofi.codecs;
